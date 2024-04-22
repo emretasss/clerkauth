66b270f4ea87e007cfbfca1406a0a20a4307f859
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-22 15:02:53 +0300

    x

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
